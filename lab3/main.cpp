6815a17c009d74c1a159b5d2e3d3bdab84a05bad
Sam Olson <olsonsd@uwec.edu>
2017-11-20 14:24:45 -0600

    Process files, and intro vector

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 #include "Process.hpp"
 
 int main() {
-	Process p(1, 30, 0, 5, 50, 0);
-	std::cout << "pid: " << p.pid << "\n";
-	std::cout << "burst: " << p.burst << "\n";
-	std::cout << "arrival: " << p.arrival << "\n";
-	std::cout << "priority: " << p.priority << "\n";
-	std::cout << "deadline: " << p.deadline << "\n";
-	std::cout << "io: " << p.io << "\n";
+	std::ifstream input("9_processes");
+
+	std::vector<Process> processes;
+
+	int pid
+		, burst
+		, arrival
+		, priority
+		, deadline
+		, io;
+
+	while (input >> pid >> burst >> arrival >> priority >> deadline >> io) {
+		Process p(pid, burst, arrival, priority, deadline, io);
+		std::cout << "pid: " << p.pid << "\n";
+		std::cout << "burst: " << p.burst << "\n";
+		std::cout << "arrival: " << p.arrival << "\n";
+		std::cout << "priority: " << p.priority << "\n";
+		std::cout << "deadline: " << p.deadline << "\n";
+		std::cout << "io: " << p.io << "\n";
+		processes.push_back(p);
+	}
 	return 0;
 }
