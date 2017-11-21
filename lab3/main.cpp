27e49a5937551d7d143929545aa6838540598293
Casey Raethke <raethkcj@uwec.edu>
2017-11-20 20:47:07 -0600

    Implemented rudimentary RTS

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -2,8 +2,31 @@
 #include <fstream>
 #include <vector>
 #include <set>
+#include <iomanip>
 
 #include "Process.hpp"
+
+void rts(std::set<Process> processes) {
+	int time = 0;
+	while(!processes.empty()) {
+		std::set<Process>::iterator p = processes.begin();
+		// Get the next unexpired process
+		while(p->deadline < (time + p->burst)) {
+			processes.erase(p);
+			p = processes.begin();
+		}
+
+		if(time >= p->arrival) {
+			std::cout << std::setw(4) << time << " -- Running " << p->pid << " with remaining burst: " << std::setw(2) << p->burst << std::endl;
+			Process tmp = *p;
+			processes.erase(p);
+			if(--tmp.burst > 0) {
+				processes.insert(tmp);
+			}
+		}
+		time++;
+	}
+}
 
 int main() {
 	std::ifstream input("9_processes");
@@ -19,18 +42,14 @@ int main() {
 
 	while (input >> pid >> burst >> arrival >> priority >> deadline >> io) {
 		Process p(pid, burst, arrival, priority, deadline, io);
-	/*	std::cout << "pid: " << p.pid << "\n";
-		std::cout << "burst: " << p.burst << "\n";
-		std::cout << "arrival: " << p.arrival << "\n";
-		std::cout << "priority: " << p.priority << "\n";
-		std::cout << "deadline: " << p.deadline << "\n";
-		std::cout << "io: " << p.io << "\n";*/
 		processes.insert(p);
 	}
 
 	for(Process p : processes) {
 		std::cout << p << "\n";
 	}
+
+	rts(processes);
 
 	return 0;
 }
