1e35a7e4953d98aaf01fa924e1ee252c8e39427f
Sam Olson <olsonsd@uwec.edu>
2017-11-25 21:20:33 -0600

    Add stubs for mfqs and whs, and add to argument check

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -8,6 +8,9 @@
 
 #include "Process.hpp"
 
+// Real-Time Scheduler
+// TODO: Account for both soft and hard RT environments
+// TODO: Report any process that cannot be scheduled (in soft RT only)
 void rts(std::set<Process, rtsCmp> processes) {
 	int time = 0;
 	int TWT = 0;
@@ -48,13 +51,32 @@ void rts(std::set<Process, rtsCmp> processes) {
 	std::cout << "AWT: " << AWT << "	ATT: " << ATT << "	NP: " << NP << std::endl;
 }
 
+// Multi-level Feedback Queue Scheduler
+// TODO: Prompt user for # of queues (up to 5), and time limit before
+//     process begins to age -- prompt here or in main?
+void mfqs(std::set<Process, rtsCmp> processes) {
+	std::cout << "MFQS coming soon!" << std::endl;
+}
+
+// Windows Hybrid Scheduler
+void whs(std::set<Process, rtsCmp> processes) {
+	std::cout << "WHS coming soon!" << std::endl;
+}
+
 int main(int argc, char **argv) {
 	// Check arguments
-	if (argc <= 1 || strcmp(argv[1], "rts") != 0) {
-		std::cout << "usage: " << argv[0] << " rts" << std::endl;
+	bool isRts
+		, isMfqs
+		, isWhs;
+
+	if (argc != 2
+		|| ((isRts = strcmp(argv[1], "rts") != 0)
+			&& (isMfqs = strcmp(argv[1], "mfqs") != 0)
+			&& (isWhs = strcmp(argv[1], "whs") != 0))
+		) {
+		std::cout << "usage: " << argv[0] << " rts|mfqs|whs" << std::endl;
 		std::cout << "For debug mode, run make clean, ";
-		std::cout << "then make debug, then ";
-		std::cout << argv[0] << " rts" << std::endl;
+		std::cout << "then make debug" << std::endl;
 		exit(1);
 	}
 
@@ -82,7 +104,12 @@ int main(int argc, char **argv) {
 	}
 #endif
 
-	rts(processes);
+	if (!isRts)
+		rts(processes);
+	else if (!isMfqs)
+		mfqs(processes);
+	else if (!isWhs)
+		whs(processes);
 
 	return 0;
 }
